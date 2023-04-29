665c133a25119add35c80123d62f609118db3820
Mario Pérez <51806979+marprezd@users.noreply.github.com>
2023-04-28 19:06:26 -0500

    Add Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,116 @@
+import * as React from 'react'
+
+function Logo(props: JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width={45}
+      height={45}
+      viewBox="0 0 11.906249 11.90625"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <defs>
+        <radialGradient
+          fx={0}
+          fy={0}
+          cx={0}
+          cy={0}
+          r={1}
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="matrix(85.86968 0 0 -85.86968 170.249 317.785)"
+          spreadMethod="pad"
+          id="a"
+        >
+          <stop offset={0} stopOpacity={1} stopColor="#1b75b9" />
+          <stop offset={0.166667} stopOpacity={1} stopColor="#1b75b9" />
+          <stop offset={0.58461496} stopOpacity={1} stopColor="#1b75b9" />
+          <stop offset={1} stopOpacity={1} stopColor="#17ddb9" />
+        </radialGradient>
+        <linearGradient
+          x1={0}
+          y1={0}
+          x2={1}
+          y2={0}
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="matrix(108.67403 0 0 -108.67403 183.286 325.776)"
+          spreadMethod="pad"
+          id="b"
+        >
+          <stop offset={0} stopOpacity={1} stopColor="#17ddb9" />
+          <stop offset={0.41538504} stopOpacity={1} stopColor="#1b75b9" />
+          <stop offset={0.833333} stopOpacity={1} stopColor="#1b75b9" />
+          <stop offset={1} stopOpacity={1} stopColor="#1b75b9" />
+        </linearGradient>
+        <linearGradient
+          x1={0}
+          y1={0}
+          x2={1}
+          y2={0}
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="matrix(-77.66992 0 0 77.66992 329.808 279.688)"
+          spreadMethod="pad"
+          id="c"
+        >
+          <stop offset={0} stopOpacity={1} stopColor="#a50054" />
+          <stop offset={0.00955605} stopOpacity={1} stopColor="#a50054" />
+          <stop offset={0.36845612} stopOpacity={1} stopColor="#a50054" />
+          <stop offset={1} stopOpacity={1} stopColor="#f38e1d" />
+        </linearGradient>
+        <linearGradient
+          x1={0}
+          y1={0}
+          x2={1}
+          y2={0}
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="matrix(-108.67403 0 0 108.67403 313.917 244.438)"
+          spreadMethod="pad"
+          id="d"
+        >
+          <stop offset={0} stopOpacity={1} stopColor="#f38e1d" />
+          <stop offset={0.63154391} stopOpacity={1} stopColor="#a50054" />
+          <stop offset={0.990444} stopOpacity={1} stopColor="#a50054" />
+          <stop offset={1} stopOpacity={1} stopColor="#a50054" />
+        </linearGradient>
+        <clipPath clipPathUnits="userSpaceOnUse" id="e">
+          <path d="M0 500h500V0H0z" />
+        </clipPath>
+      </defs>
+      <path
+        d="M171.921 324.836c-6.033-6.032-6.032-15.814 0-21.846l27.669-27.602 19.106-19.172c6.032-6.033 15.812-6.033 21.845 0 6.033 6.03 6.033 15.813 0 21.843l-46.775 46.777a15.4 15.4 0 01-10.923 4.524 15.396 15.396 0 01-10.922-4.524"
+        fill="url(#a)"
+        stroke="none"
+        transform="matrix(.06793 0 0 -.06174 -10.963 23.622)"
+      />
+      <path
+        d="M218.695 371.64l-46.774-46.777c-5.908-5.906-6.278-15.149-.618-21.205l.24-.254.311-.312c.024-.023.043-.049.067-.073.024-.025.052-.046.077-.071l27.592-27.56c-14.857 14.858 5.09 38.544 5.09 38.544l-.004-.001 35.865 35.863c6.031 6.033 6.033 15.815 0 21.846a15.398 15.398 0 01-10.922 4.524 15.402 15.402 0 01-10.924-4.524"
+        fill="url(#b)"
+        stroke="none"
+        transform="matrix(.06793 0 0 -.06174 -10.963 23.622)"
+      />
+      <path
+        d="M256.661 313.998c-6.031-6.032-6.031-15.813 0-21.844l46.777-46.777c6.032-6.032 15.814-6.034 21.845 0 6.033 6.033 6.031 15.814 0 21.846l-27.671 27.602-19.104 19.173a15.403 15.403 0 01-10.924 4.525 15.399 15.399 0 01-10.923-4.525"
+        fill="url(#c)"
+        stroke="none"
+        transform="matrix(.06793 0 0 -.06174 -10.963 23.622)"
+      />
+      <path
+        d="M292.523 256.281l.003.002-35.864-35.865c-6.032-6.032-6.032-15.813-.001-21.844 6.034-6.032 15.812-6.031 21.847 0l46.775 46.776c5.906 5.906 6.277 15.15.616 21.205l-.239.256-.312.311c-.021.023-.041.049-.065.073-.024.025-.054.046-.079.07l-27.592 27.56c14.859-14.858-5.089-38.544-5.089-38.544"
+        fill="url(#d)"
+        stroke="none"
+        transform="matrix(.06793 0 0 -.06174 -10.963 23.622)"
+      />
+      <g clipPath="url(#e)" fill="#e6e6e6" transform="matrix(.06793 0 0 -.06174 -10.963 23.622)">
+        <path
+          d="M0 0c-2.466-7.884-10.854-12.275-18.738-9.81-7.881 2.465-12.273 10.856-9.808 18.738l48.382 152.079c2.467 7.884 10.857 12.277 18.74 9.81 7.881-2.464 12.274-10.855 9.809-18.738z"
+          fill="#e6e6e6"
+          fillOpacity={1}
+          fillRule="nonzero"
+          stroke="none"
+          transform="translate(239.037 204.732)"
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default Logo
